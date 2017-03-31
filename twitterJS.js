30f98794d08b624cd62ba83f8b6daa57b3b83e5f
[Steve Wallace] <[swallace35@gmail.com]>
2017-03-31 00:37:37 -0400

    New Button working

--- twitterJS.js
+++ twitterJS.js
@@ -1,7 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+$(document).ready(function(){
+var randomQuotes = [
+    "It's hammer time!",
+    "The sky is falling",
+    "Cocaine is a hell of a drug",
+    "Turqoise Rain",
+    "And yadda yadda yadda...",
+    "Pretty, pretty, pretty good",
+    "Oh... yeah!",
+    "These pretzels are making me thirsty",
+    "You are a very bad man",
+    "The Airing of Grievences",
+    "The other white meat"
+];
 
+$("#newButton").on('click', function() {
+    var x = Math.floor(Math.random() * randomQuotes.length);
+    $("#text").text(randomQuotes[x]);
+});
+
+});
+/*
+$(document).ready(function () {
+ $('#btnTweet').click(function (e) {
+ alert('hello11');
+ var textToTweet = "Hi I am tweeting from here";
+ if (textToTweet.length > 140) {
+ alert('Tweet should be less than 140 Chars');
+ }
+ else {
+ var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(textToTweet);
+ window.open(twtLink,'_blank');
+ }
+ });
+ });
+*/
